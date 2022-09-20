1d91c5c52a547bb6d02d42c659bf116f609f1e1c
joshua <joshuameasurehughes@gmail.com>
2022-09-20 19:41:57 +0100

    vertex buffer done

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+
 // linear algebra
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
+// #define GLM_FORCE_RADIANS
+// #define GLM_FORCE_DEPTH_ZERO_TO_ONE
+// #include <glm/vec4.hpp>
+// #include <glm/mat4x4.hpp>
 
 // error handling
 #include <iostream>
@@ -21,9 +23,13 @@
 #include <set>
 #include <limits>
 #include <fstream>
+#include <array>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+
+// allow rendering of one frame to not interfere with the recording of the next
+const int MAX_FRAMES_IN_FLIGHT = 2;
 
 const std::vector<const char*> validationLayers = {
 	"VK_LAYER_KHRONOS_validation"
@@ -84,6 +90,40 @@ struct SwapChainSupportDetails {
 	std::vector<VkPresentModeKHR> presentModes;
 };
 
+struct Vertex {
+	glm::vec2 pos;
+	glm::vec3 color;
+
+	static VkVertexInputBindingDescription getBindingDescription() {
+		VkVertexInputBindingDescription bindingDescription{};
+		bindingDescription.binding = 0;
+		bindingDescription.stride = sizeof(Vertex);
+		bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+		return bindingDescription;
+	}
+
+	static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
+		std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
+		attributeDescriptions[0].binding = 0;
+		attributeDescriptions[0].location = 0;
+		attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+		attributeDescriptions[0].offset = offsetof(Vertex, pos);
+		attributeDescriptions[1].binding = 0;
+		attributeDescriptions[1].location = 1;
+		attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+		attributeDescriptions[1].offset = offsetof(Vertex, color);
+
+		return attributeDescriptions;
+	}
+};
+
+const std::vector<Vertex> vertices = {
+	{{0.0f, -0.5f}, {1.0f, 1.0f, 1.0f}},
+	{{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+	{{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
+};
+
 class HelloTriangleApplication {
 	public:
 		void run() {
@@ -120,11 +160,16 @@ class HelloTriangleApplication {
 
 		std::vector<VkFramebuffer> swapChainFramebuffers;
 		VkCommandPool commandPool;
-		VkCommandBuffer commandBuffer;
+		// one for each frame in flight
+		std::vector<VkCommandBuffer> commandBuffers;
+		std::vector<VkSemaphore> imageAvailableSemaphores;
+		std::vector<VkSemaphore> renderFinishedSemaphores;
+		std::vector<VkFence> inFlightFences;
+		uint32_t currentFrame = 0;
+		bool framebufferResized = false;
 
-		VkSemaphore imageAvailableSemaphore;
-		VkSemaphore renderFinishedSemaphore;
-		VkFence inFlightFence;
+		VkBuffer vertexBuffer;
+		VkDeviceMemory vertexBufferMemory;
 
 		// initialise GLFW and create a window
 		void initWindow() {
@@ -133,12 +178,17 @@ class HelloTriangleApplication {
 			// do not create OpenGL context (default behaviour)
 			glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 			// disable window resizing
-			glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		//	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
 			// create window (width, height, title, specify monitor [opt], openGL specific param)
 			window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+			glfwSetWindowUserPointer(window, this);
+			glfwSetFramebufferSizeCallback(window, framebufferResizeCallback);
+		}
 
-
+		static void framebufferResizeCallback(GLFWwindow* window, int width, int height) {
+			auto app = reinterpret_cast<HelloTriangleApplication*>(glfwGetWindowUserPointer(window));
+			app->framebufferResized = true;
 		}
 
 		void initVulkan() {
@@ -153,7 +203,8 @@ class HelloTriangleApplication {
 			createGraphicsPipeline();
 			createFramebuffers();
 			createCommandPool();
-			createCommandBuffer();
+			createVertexBuffer();
+			createCommandBuffers();
 			createSyncObjects();
 		}
 
@@ -168,23 +219,21 @@ class HelloTriangleApplication {
 		}
 
 		void cleanup() {
-			vkDestroySemaphore(device, imageAvailableSemaphore, nullptr);
-			vkDestroySemaphore(device, renderFinishedSemaphore, nullptr);
-			vkDestroyFence(device, inFlightFence, nullptr);
+			cleanupSwapChain();
+
+			vkDestroyBuffer(device, vertexBuffer, nullptr);
+			vkFreeMemory(device, vertexBufferMemory, nullptr);
+
+			for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
+				vkDestroySemaphore(device, imageAvailableSemaphores[i], nullptr);
+				vkDestroySemaphore(device, renderFinishedSemaphores[i], nullptr);
+				vkDestroyFence(device, inFlightFences[i], nullptr);
+			}
 
 			vkDestroyCommandPool(device, commandPool, nullptr);
-			for (auto framebuffer : swapChainFramebuffers) {
-				vkDestroyFramebuffer(device, framebuffer, nullptr);
-			}
 			vkDestroyPipeline(device, graphicsPipeline, nullptr);
 			vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
 			vkDestroyRenderPass(device, renderPass, nullptr);
-
-			for (auto imageView : swapChainImageViews) {
-				vkDestroyImageView(device, imageView, nullptr);
-			}
-
-			vkDestroySwapchainKHR(device, swapChain, nullptr);
 
 			// instance not a parameter as logical devices do not interact directly with instances
 			vkDestroyDevice(device, nullptr);
@@ -202,6 +251,78 @@ class HelloTriangleApplication {
 			glfwTerminate();
 		}
 
+		void cleanupSwapChain() {
+			for (auto framebuffer : swapChainFramebuffers) {
+				vkDestroyFramebuffer(device, framebuffer, nullptr);
+			}
+			for (auto imageView : swapChainImageViews) {
+				vkDestroyImageView(device, imageView, nullptr);
+			}
+			vkDestroySwapchainKHR(device, swapChain, nullptr);
+
+		}
+
+		void recreateSwapChain() {
+			int width = 0, height = 0;
+			glfwGetFramebufferSize(window, &width, &height);
+			// minimized window size = 0
+			while (width == 0 || height == 0) {
+				glfwGetFramebufferSize(window, &width, &height);
+				glfwWaitEvents();
+			}
+				
+			vkDeviceWaitIdle(device);
+
+			cleanupSwapChain();
+
+			createSwapChain();
+			createImageViews();
+			createFramebuffers();
+		}
+
+		void createVertexBuffer() {
+			VkBufferCreateInfo bufferInfo{};
+			bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+			bufferInfo.size = sizeof(vertices[0]) * vertices.size();
+			bufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+			bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+			if (vkCreateBuffer(device, &bufferInfo, nullptr, &vertexBuffer) != VK_SUCCESS) {
+				throw std::runtime_error("failed to create vertex buffer.");
+			}
+
+			VkMemoryRequirements memRequirements;
+			vkGetBufferMemoryRequirements(device, vertexBuffer, &memRequirements);
+
+			VkMemoryAllocateInfo allocInfo{};
+			allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+			allocInfo.allocationSize = memRequirements.size;
+			allocInfo.memoryTypeIndex = findMemoryType(memRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+
+			if (vkAllocateMemory(device, &allocInfo, nullptr, &vertexBufferMemory) != VK_SUCCESS) {
+				throw std::runtime_error("failed to allocate vertex buffer memory.");
+			}
+
+			vkBindBufferMemory(device, vertexBuffer, vertexBufferMemory, 0);
+
+			void* data;
+			vkMapMemory(device, vertexBufferMemory, 0, bufferInfo.size, 0, &data);
+			memcpy(data, vertices.data(), (size_t) bufferInfo.size);
+			vkUnmapMemory(device, vertexBufferMemory);
+		}
+
+		uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties) {
+			VkPhysicalDeviceMemoryProperties memProperties;
+			vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
+
+			for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+				if ((typeFilter & (1 << i)) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {
+					return i;
+				}
+			}
+			throw std::runtime_error("failed to find suitable memory type.");
+		}
+
 		void drawFrame() {
 			/* rendering frame steps:
 			 * 	- wait for previous frame to finish
@@ -212,32 +333,39 @@ class HelloTriangleApplication {
 			 */
 
 			// wait for previous frame
-			vkWaitForFences(device, 1, &inFlightFence, VK_TRUE, UINT64_MAX);
-			// reset fence after waiting
-			vkResetFences(device, 1, &inFlightFence);
+			vkWaitForFences(device, 1, &inFlightFences[currentFrame], VK_TRUE, UINT64_MAX);
 
 			// acqure image from swap chain
 			uint32_t imageIndex;
-			vkAcquireNextImageKHR(device, swapChain, UINT64_MAX, imageAvailableSemaphore, VK_NULL_HANDLE, &imageIndex);
+			VkResult result = vkAcquireNextImageKHR(device, swapChain, UINT64_MAX, imageAvailableSemaphores[currentFrame], VK_NULL_HANDLE, &imageIndex);
+			if (result == VK_ERROR_OUT_OF_DATE_KHR) {
+				recreateSwapChain();
+				return;
+			} else if (result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR) {
+				throw std::runtime_error("failed to acquire swap chain image.");
+			}
+			// reset fence after waiting & confirming carrying on
+			vkResetFences(device, 1, &inFlightFences[currentFrame]);
+
 			// record a command buffer
-			vkResetCommandBuffer(commandBuffer, 0);
-			recordCommandBuffer(commandBuffer, imageIndex);
+			vkResetCommandBuffer(commandBuffers[currentFrame], 0);
+			recordCommandBuffer(commandBuffers[currentFrame], imageIndex);
 
 			// submit the command buffer
 			VkSubmitInfo submitInfo{};
 			submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
-			VkSemaphore waitSemaphores[] = {imageAvailableSemaphore};
+			VkSemaphore waitSemaphores[] = {imageAvailableSemaphores[currentFrame]};
 			VkPipelineStageFlags waitStages[] = {VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
 			submitInfo.waitSemaphoreCount = 1;
 			submitInfo.pWaitSemaphores = waitSemaphores;
 			submitInfo.pWaitDstStageMask = waitStages;
 			submitInfo.commandBufferCount = 1;
-			submitInfo.pCommandBuffers = &commandBuffer;
-			VkSemaphore signalSemaphores[] = {renderFinishedSemaphore};
+			submitInfo.pCommandBuffers = &commandBuffers[currentFrame];
+			VkSemaphore signalSemaphores[] = {renderFinishedSemaphores[currentFrame]};
 			submitInfo.signalSemaphoreCount = 1;
 			submitInfo.pSignalSemaphores = signalSemaphores;
 
-			if (vkQueueSubmit(graphicsQueue, 1, &submitInfo, inFlightFence) != VK_SUCCESS) {
+			if (vkQueueSubmit(graphicsQueue, 1, &submitInfo, inFlightFences[currentFrame]) != VK_SUCCESS) {
 				throw std::runtime_error("failed to submit draw command buffer.");
 			}
 
@@ -253,10 +381,23 @@ class HelloTriangleApplication {
 			presentInfo.pImageIndices = &imageIndex;
 			presentInfo.pResults = nullptr;
 
-			vkQueuePresentKHR(presentQueue, &presentInfo);
+			result = vkQueuePresentKHR(presentQueue, &presentInfo);
+
+			if (result == VK_ERROR_OUT_OF_DATE_KHR || result == VK_SUBOPTIMAL_KHR || framebufferResized) {
+				framebufferResized = false;
+				recreateSwapChain();
+			} else if (result != VK_SUCCESS) {
+				throw std::runtime_error("failed to present swap chain image.");
+			}
+
+			currentFrame = (currentFrame + 1) % MAX_FRAMES_IN_FLIGHT;
 		}
 
 		void createSyncObjects() {
+			imageAvailableSemaphores.resize(MAX_FRAMES_IN_FLIGHT);
+			renderFinishedSemaphores.resize(MAX_FRAMES_IN_FLIGHT);
+			inFlightFences.resize(MAX_FRAMES_IN_FLIGHT);
+
 			VkSemaphoreCreateInfo semaphoreInfo{};
 			semaphoreInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
 
@@ -264,21 +405,25 @@ class HelloTriangleApplication {
 			fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
 			fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
 
-			if (vkCreateSemaphore(device, &semaphoreInfo, nullptr, &imageAvailableSemaphore) != VK_SUCCESS ||
-			    vkCreateSemaphore(device, &semaphoreInfo, nullptr, &renderFinishedSemaphore) != VK_SUCCESS ||
-			    vkCreateFence(device, &fenceInfo, nullptr, &inFlightFence) != VK_SUCCESS) {
-				throw std::runtime_error("failed to create sync objects.");
+			for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
+				if (vkCreateSemaphore(device, &semaphoreInfo, nullptr, &imageAvailableSemaphores[i]) != VK_SUCCESS ||
+				    vkCreateSemaphore(device, &semaphoreInfo, nullptr, &renderFinishedSemaphores[i]) != VK_SUCCESS ||
+				    vkCreateFence(device, &fenceInfo, nullptr, &inFlightFences[i]) != VK_SUCCESS) {
+					throw std::runtime_error("failed to create sync objects for a frame.");
+				}
 			}
 		}
 
-		void createCommandBuffer() {
+		void createCommandBuffers() {
+			commandBuffers.resize(MAX_FRAMES_IN_FLIGHT);
+
 			VkCommandBufferAllocateInfo allocInfo{};
 			allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
 			allocInfo.commandPool = commandPool;
 			allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-			allocInfo.commandBufferCount = 1;
+			allocInfo.commandBufferCount = (uint32_t) commandBuffers.size();
 
-			if (vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer) != VK_SUCCESS) {
+			if (vkAllocateCommandBuffers(device, &allocInfo, commandBuffers.data()) != VK_SUCCESS) {
 				throw std::runtime_error("failed to allocate command buffers.");
 			}
 		}
@@ -308,6 +453,11 @@ class HelloTriangleApplication {
 			vkCmdBeginRenderPass(commandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
 			// bind the graphics pipeline (2nd argument specifies graphics or compute pipeline)
 			vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline);
+			
+			VkBuffer vertexBuffers[] = {vertexBuffer};
+			VkDeviceSize offsets[] = {0};
+			vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers, offsets);
+
 			// as scissor and viewport dynamic, set here
 			VkViewport viewport{};
 			viewport.x = 0.0f;
@@ -323,7 +473,7 @@ class HelloTriangleApplication {
 			vkCmdSetScissor(commandBuffer, 0, 1, &scissor);
 
 			// draw command for triangle (buffer, vertexCount, instanceCount, firstVertex, firstInstance)
-			vkCmdDraw(commandBuffer, 3, 1, 0, 0);
+			vkCmdDraw(commandBuffer, static_cast<uint32_t>(vertices.size()), 1, 0, 0);
 
 			vkCmdEndRenderPass(commandBuffer);
 
@@ -444,10 +594,16 @@ class HelloTriangleApplication {
 			VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
 			vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
 			// hard-coding vertex data directly into vertex shader so no vertex data
-			vertexInputInfo.vertexBindingDescriptionCount = 0;
-			vertexInputInfo.pVertexBindingDescriptions = nullptr; // Optional
-			vertexInputInfo.vertexAttributeDescriptionCount = 0;
-			vertexInputInfo.pVertexAttributeDescriptions = nullptr; // Optional
+			// vertexInputInfo.vertexBindingDescriptionCount = 0;
+			// vertexInputInfo.pVertexBindingDescriptions = nullptr; // Optional
+			// vertexInputInfo.vertexAttributeDescriptionCount = 0;
+			// vertexInputInfo.pVertexAttributeDescriptions = nullptr; // Optional
+			auto bindingDescription = Vertex::getBindingDescription();
+			auto attributeDescriptions = Vertex::getAttributeDescriptions();
+			vertexInputInfo.vertexBindingDescriptionCount = 1;
+			vertexInputInfo.vertexAttributeDescriptionCount = static_cast<uint32_t>(attributeDescriptions.size());
+			vertexInputInfo.pVertexBindingDescriptions = &bindingDescription;
+			vertexInputInfo.pVertexAttributeDescriptions = attributeDescriptions.data();
 
 			// Input assembly
 			// what kind of geometry will be drawn from the vertices & if the primitive restart should be enabled
